4e3bd88e9aafcb28f2f3ad5347c7e76183c26251
Daniel Baracaldo <danielbaracaldoc.395@gmail.com>
2023-10-07 00:52:57 -0500

    Product API connection

--- index.js
+++ index.js
@@ -1,45 +1,75 @@
 //Variable definition
-const url = 'http://localhost:3000/api/articulos/'
-const container = document.getElementById('tbody') 
-const modalArticle = new bootstrap.Modal(document.getElementById('modalArticle'))
-const form = document.getElementById('modal-form')
-const nameArticle = document.getElementById('name')
-const price = document.getElementById('price')
-const stock = document.getElementById('stock')
-const btnCreate = document.getElementById('btnCreate')
+const url = "http://localhost:3000/api/products/";
+const container = document.getElementById("tbody");
+const modalArticle = new bootstrap.Modal(
+  document.getElementById("modalArticle")
+);
+const form = document.getElementById("modal-form");
+const nameArticle = document.getElementById("name");
+const price = document.getElementById("price");
+const stock = document.getElementById("stock");
+const btnCreate = document.getElementById("btnCreate");
 
+let results = "";
+let option = "";
 
-let results = ''
-let option = ''
-console.log(stock);
-
-btnCreate.addEventListener('click', () => {
-  nameArticle.value = ''
-  price.value = ''
-  stock.value = ''
-  modalArticle.show()
-  option = 'create'
-})
-
+btnCreate.addEventListener("click", () => {
+  nameArticle.value = "";
+  price.value = "";
+  stock.value = "";
+  modalArticle.show();
+  option = "create";
+});
 
 //Function show results
-const display = (articles)=> {
-  articles.forEach(article => {
-    results = `<tr>
+const display = (articles) => {
+  articles.forEach((article) => {
+    results += `<tr>
                   <td>${article.id}</td>
-                  <td>${article.nameArticle}</td>
-                  <td>${article.price}</td>
+                  <td>${article.nameProduct}</td>
+                  <td>$${article.price}</td>
                   <td>${article.stock}</td>
-                  <td class="text-center"><a class="btnEdit btn btn-primary">Edit</a> <a class="btnDelete btn btn-primary">Delet</a></td>
+                  <td class="text-center d-flex justify-content-evenly"><a class="btnEdit btn btn-primary">Edit</a> <a class="btnDelete btn btn-danger">Delet</a></td>
 
-              </tr>`
+              </tr>`;
   });
 
-  container.innerHTML = results
-}
+  container.innerHTML = results;
+};
 
 // Modal procedure
-fetch(url) 
-  .then(response => response.json())
-  .then(data => display(data)) 
-  .catch(error => console.log(error))
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => display(data))
+  .catch((error) => console.log(error));
+
+const on = (element, eventt, selector, handler) => {
+  element.addEventListener(eventt, (e) => {
+    if (e.target.closest(selector)) {
+      handler(e);
+    }
+  });
+};
+
+// Deletion procedure
+on(document, "click", ".btnDelete", (e) => {
+  const row = e.target.parentNode.parentNode;
+  const id = row.firstElementChild.innerHTML;
+
+  alertify.confirm(
+    "This is a confirm dialog.",
+    function () {
+      
+      fetch(url + id, {
+        method: "DELETE", 
+      })
+        .then((res) => res.json()) 
+        .then(() => location.reload()) 
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
+    function () {
+      alertify.error("Cancel");
+    })
+});
